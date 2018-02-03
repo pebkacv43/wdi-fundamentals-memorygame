6309c8b48742c7fb34ea73383b7b1c7a3a58a52e
KC Bouma <pebkacv43.0@gmail.com>
2018-02-03 11:51:06 -0800

    added js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!")
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// alert("Up and running!");
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped a queen!");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped a king!");
+
+if ((cardsInPlay.length===2) && (cardsInPlay[0]===cardsInPlay[1])) {
+    alert("Holy Schnike's! You found a match!")
+  }
+    else {
+    alert("Sorry, try again girlfriend!")
+  }
