6238160414ba04f8ab82a38d7cf4b08b3358d2ee
KC Bouma <pebkacv43.0@gmail.com>
2018-02-06 13:18:29 -0800

    added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,27 @@
 // alert("Up and running!");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
+var checkForMatch = function() {
+  if ((cardsInPlay.length===2) && (cardsInPlay[0]===cardsInPlay[1])) {
+      alert("Holy Schnike's! You found a match!")
+    }
+      else {
+      alert("Sorry, try again girlfriend!")
+    }
+}
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+  // var cardOne = cards[0];
+  // cardsInPlay.push(cardOne);
+  // console.log("user flipped a queen!");
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped a queen!");
+  // var cardTwo = cards[2];
+  // cardsInPlay.push(cardTwo);
+  // console.log("User flipped a king!");
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped a king!");
 
-if ((cardsInPlay.length===2) && (cardsInPlay[0]===cardsInPlay[1])) {
-    alert("Holy Schnike's! You found a match!")
-  }
-    else {
-    alert("Sorry, try again girlfriend!")
-  }
+}
+flipCard(0);
+flipCard(2);
